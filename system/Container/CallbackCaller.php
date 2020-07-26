b5bc86c902336ae3ce4dc1b4a9e055b51c23b2e1
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2020-07-26 20:52:03 +0300

    Improve the Mini\Container\CallbackCaller

--- system/Container/CallbackCaller.php
+++ system/Container/CallbackCaller.php
@@ -2,8 +2,8 @@
 
 namespace Mini\Container;
 
-use Mini\Support\Str;
 use Mini\Container\Container;
+use Mini\Support\Str;
 
 use Closure;
 use InvalidArgumentException;
@@ -21,28 +21,50 @@ class CallbackCaller
      * @param  callable|string  $callback
      * @param  array  $parameters
      * @param  string|null  $defaultMethod
+     * @throws \InvalidArgumentException
      * @return mixed
      */
     public static function call(Container $container, $callback, array $parameters = array(), $defaultMethod = null)
     {
         if (is_string($callback)) {
-            list ($className, $method) = Str::parseCallback($callback, $defaultMethod);
-
-            if (empty($method) || ! class_exists($className)) {
-                throw new InvalidArgumentException('Invalid callback provided.');
-            }
-
-            $callback = array($container->make($className), $method);
+            $callback = static::resolveStringCallback($container, $callback, $defaultMethod);
         }
 
-        if (is_array($callback)) {
-            $reflector = new ReflectionMethod($callback[0], $callback[1]);
-        } else {
+        if ($callback instanceof Closure) {
             $reflector = new ReflectionFunction($callback);
+        }
+
+        //
+        else if (is_array($callback)) {
+            $reflector = new ReflectionMethod($callback[0], $callback[1]);
+        }  else {
+            throw new InvalidArgumentException('Invalid callback provided.');
         }
 
         return call_user_func_array(
             $callback, static::getMethodDependencies($container, $parameters, $reflector)
+        );
+    }
+
+    /**
+     * Resolve a string callback.
+     *
+     * @param  \Mini\Container \Container $container
+     * @param  string  $callback
+     * @param  string|null  $defaultMethod
+     * @throws \InvalidArgumentException
+     * @return array
+     */
+    protected static function resolveStringCallback(Container $container, $callback, $defaultMethod = null)
+    {
+        list ($className, $method) = Str::parseCallback($callback, $defaultMethod);
+
+        if (empty($method) || ! class_exists($className)) {
+            throw new InvalidArgumentException('Invalid callback provided.');
+        }
+
+        return array(
+            $container->make($className), $method
         );
     }
 
@@ -67,7 +89,9 @@ class CallbackCaller
 
             // The dependency does not exists in parameters.
             else if (! is_null($class = $parameter->getClass())) {
-                $dependencies[] = $container->make($class->getName());
+                $className = $class->getName();
+
+                $dependencies[] = $container->make($className);
             }
 
             // The dependency does not reference a class.
