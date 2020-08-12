706b0281f9f342343f887fdf0181d51af29666a5
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2020-08-12 11:13:49 +0300

    Improve the Routing

--- system/Routing/Route.php
+++ system/Routing/Route.php
@@ -9,9 +9,6 @@ use Mini\Support\Str;
 
 use Closure;
 use LogicException;
-use ReflectionFunction;
-use ReflectionFunctionAbstract;
-use ReflectionMethod;
 
 
 class Route
@@ -138,41 +135,13 @@ class Route
     public function run(Request $request)
     {
         try {
-            if (is_array($callback = $this->resolveActionCallback())) {
-                return $this->runControllerAction($callback, $request);
-            }
+            $callback = $this->resolveActionCallback();
 
-            $parameters = $this->resolveCallParameters(
-                $this->getParameters(), new ReflectionFunction($callback)
-            );
-
-            return call_user_func_array($callback, $parameters);
+            return with(new CallbackCaller($this->container))->call($callback, $this->getParameters(), $request);
         }
         catch (HttpResponseException $e) {
             return $e->getResponse();
         }
-    }
-
-    /**
-     * Runs the controller callback and returns the response.
-     *
-     * @param  array  $callback
-     * @param  \Mini\Http\Request  $request
-     * @return mixed
-     */
-    protected function runControllerAction(array $callback, Request $request)
-    {
-        extract($callback);
-
-        $parameters = $this->resolveCallParameters(
-            $this->getParameters(), new ReflectionMethod($controller, $method)
-        );
-
-        if (method_exists($controller, 'callAction')) {
-            return $controller->callAction($method, $parameters, $request);
-        }
-
-        return call_user_func_array($callback, $parameters);
     }
 
     /**
@@ -207,50 +176,6 @@ class Route
         }
 
         return $this->callback = compact('controller', 'method');
-    }
-
-    /**
-     * Resolve the given method's type-hinted dependencies.
-     *
-     * @param  array  $parameters
-     * @param  \ReflectionFunctionAbstract  $reflector
-     * @return array
-     */
-    protected function resolveCallParameters(array $parameters, ReflectionFunctionAbstract $reflector)
-    {
-        $count = 0;
-
-        $values = array_values($parameters);
-
-        foreach ($reflector->getParameters() as $key => $parameter) {
-            if (! is_null($class = $parameter->getClass())) {
-                $className = $class->getName();
-
-                $this->spliceIntoParameters($parameters, $key, $this->container->make($className));
-
-                $count++;
-            }
-
-            //
-            else if (! isset($values[$key - $count]) && $parameter->isDefaultValueAvailable()) {
-                $this->spliceIntoParameters($parameters, $key, $parameter->getDefaultValue());
-            }
-        }
-
-        return array_values($parameters);
-    }
-
-    /**
-     * Splice the given value into the parameter list.
-     *
-     * @param  array  $parameters
-     * @param  string  $offset
-     * @param  mixed  $value
-     * @return void
-     */
-    protected function spliceIntoParameters(array &$parameters, $offset, $value)
-    {
-        array_splice($parameters, $offset, 0, array($value));
     }
 
     /**
